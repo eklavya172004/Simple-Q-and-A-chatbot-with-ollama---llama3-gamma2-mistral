15585229d1e35307316896c47cc64264ff4f11bf
eklavya172004 <eklavya172004@gmail.com>
2026-05-19 19:32:53 +0530

    Trigger redeploy for dependency installation

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ os.environ['LANGCHAIN_API_KEY']=os.getenv("LANGCHAIN_API_KEY")
 os.environ['LANGCHAIN_TRACING_V2']="true"
 os.environ['LANGCHAIN_PROJECT']="simpleQNA"
 
-
 # Prompt Template
 prompt=ChatPromptTemplate.from_messages(
     [
